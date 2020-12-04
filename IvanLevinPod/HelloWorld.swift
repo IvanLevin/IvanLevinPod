6239c1354eb75371b2eea28eaafa008bdbae0420
Ivan Levin <ilevin@alfabank.ru>
2020-12-05 00:08:51 +0300

    Добавлен файл Hello World

---
+++ IvanLevinPod/HelloWorld.swift
@@ -0,0 +1,14 @@
+//
+//  HelloWorld.swift
+//  IvanLevinPod
+//
+//  Created by Ivan Levin on 05.12.2020.
+//
+
+import Foundation
+
+class HelloWorld {
+    func getHelloWorld() -> String {
+        "Hello world!"
+    }
+}
---
+++ IvanLevinPod/IvanLevinPod.h
@@ -0,0 +1,18 @@
+//
+//  IvanLevinPod.h
+//  IvanLevinPod
+//
+//  Created by Ivan Levin on 05.12.2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IvanLevinPod.
+FOUNDATION_EXPORT double IvanLevinPodVersionNumber;
+
+//! Project version string for IvanLevinPod.
+FOUNDATION_EXPORT const unsigned char IvanLevinPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IvanLevinPod/PublicHeader.h>
+
+
